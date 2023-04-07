18fa9525f8395de64620ce789794fd5852fa76aa
Pasindu Priyashan <47271150+bataishere@users.noreply.github.com>
2023-04-07 17:39:10 +0530

    init

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,13 @@
+<footer class="footer py-5">
+    <div class="container">
+      <div class="row mt-5">
+        <div class="col-8 mx-auto text-center mt-1">
+          <p class="mb-0 text-secondary">
+            Copyright © <script>
+              document.write(new Date().getFullYear())
+            </script> {{ config('app.name', 'Laravel') }}, Powered By Aries.
+          </p>
+        </div>
+      </div>
+    </div>
+  </footer>
